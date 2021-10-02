b23e265a54bed1aa6bd909fc4646907b926fa080
Gabriel Figueira <gabriel.h.figueira@gmail.com>
2021-10-02 13:08:36 -0300

    Criação da estrutura do projeto de cadastro de pontos turisticos

---
+++ SistemaCadastroLocais/Models/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SistemaCadastroLocais.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Locais> Locais { get; set; }
+
+        public Contexto(DbContextOptions<Contexto>opcoes): base(opcoes)
+        { }
+    }
+}
